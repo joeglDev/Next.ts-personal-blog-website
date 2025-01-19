4d7d33eb5f6babfc824a84e6e8aaf9540b426cb5
hiroji <joegilbertdev@outlook.com>
2025-01-19 21:39:49 +0000

    feature/store-posts-in-reducer
    init blog posts in reducer

--- pages/MainView.tsx
+++ pages/MainView.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import {useContext, useEffect, useReducer} from "react";
 import { ThemeContainer } from "../components/ThemeContainer";
 import { context } from "../components/Context";
 import { SidePanel } from "../components/mainview/SidePanel/SidePanel";
@@ -8,9 +8,26 @@ import {
   MainViewItemWrapper,
   MainViewWrapper,
 } from "../components/mainview/MainView.style";
+import {blogPostsReducer} from "../components/BlogPostsReducer";
+import {getBlogPostServerProps} from "../lib/blog-posts/blogPostController";
 
 export default function MainView() {
   const { lightMode } = useContext(context);
+
+  const [state, dispatch] = useReducer(blogPostsReducer, {blogPosts: []});
+
+  useEffect(() => {
+      const fetchBlogPosts = async () => {
+            try {
+                const data = await getBlogPostServerProps();
+                dispatch({ type: "SET_BLOG_POSTS", blogPosts: [...data] });
+            } catch (error) {
+                console.error("Failed to fetch blog posts:", error);
+            }
+        };
+
+        fetchBlogPosts();
+    }, []);
 
   return (
     <ThemeContainer lightMode={lightMode}>
@@ -20,7 +37,7 @@ export default function MainView() {
           <SidePanel />
         </MainViewItemWrapper>
         <MainViewItemWrapper width={75}>
-          <Feed />
+          <Feed blogPosts={state.blogPosts} />
         </MainViewItemWrapper>
       </MainViewWrapper>
     </ThemeContainer>
