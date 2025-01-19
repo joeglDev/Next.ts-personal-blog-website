344008c70f3685bb7858210bf62dd1bbf4401977
hiroji <joegilbertdev@outlook.com>
2025-01-19 21:49:46 +0000

    feature/posts-in-reducer
    moved Action.type to enum

--- components/BlogPostsReducer.ts
+++ components/BlogPostsReducer.ts
@@ -1,7 +1,11 @@
 import {BlogPost} from "./mainview/feed/Feed.types";
 
+export enum ActionTypes {
+    SET_BLOG_POST = "SET_BLOG_POST",
+}
+
 interface SetBlogPostsAction {
-    type: "SET_BLOG_POSTS";
+    type: ActionTypes.SET_BLOG_POST;
     blogPosts: BlogPost[];
 }
 
@@ -13,7 +17,7 @@ interface State {
 
 export const blogPostsReducer = (state: State, action: Actions): State => {
     switch (action.type) {
-        case "SET_BLOG_POSTS":
+        case ActionTypes.SET_BLOG_POST:
             return {
                 blogPosts: action.blogPosts,
             }
